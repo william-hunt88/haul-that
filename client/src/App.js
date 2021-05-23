8bb39898c56c8797cad8624e8bec9bfe4f21d56d
Your NameAlex <alehr45@gmail.com>
2021-05-22 23:13:52 -0500

    nav bar bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
       
       {/* <Home /> */}
       
-      {/* <Profile /> */}
+      <Profile />
       
     </div>
   );
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,40 +2,27 @@ import React, { useState } from "react";
 import { PageHeader } from "antd";
 import { UserOutlined, MenuOutlined } from "@ant-design/icons";
 
-function Nav() {
-  const [navbarOpen, setNavbarOpen] = useState(false);
 
-  const handleToggle = () => {
-    setNavbarOpen((prev) => !prev);
-  };
+
+function Nav() {
 
   return (
-    <nav className="navBar">
-      <PageHeader
-        className="site-page-header"
-        title="HaulThat"
-        extra={[<UserOutlined class="profile" key="3"></UserOutlined>]}
-      />
+<div class="navbar">
+  
+  
+  <div class="dropdown">
+    <button class="dropbtn"><MenuOutlined />
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="#">Booking</a>
+      <a href="#">Jobs</a>
+      <a href="#">My Account</a>
+    </div>
+  </div>
+ <h1 class="title">HaulThat</h1>
+</div>
+  )}
 
-      <button className="buttonsmall" onClick={handleToggle}>
-        {navbarOpen ? (
-          <MenuOutlined
-            style={{ color: "#ffffff", width: "40px", height: "40px" }}
-          />
-        ) : (
-          <MenuOutlined
-            style={{ color: "#ffffff", width: "40px", height: "40px" }}
-          />
-        )}</button>
-
-      <ul class="menu1" className={`menuNav ${navbarOpen ? " showMenu" : ""}`}>
-        <li class="booking">Booking</li>
-        <li class="jobs">Jobs</li>
-        <li class="account">Account</li>
-        
-      </ul>
-    </nav>
-  );
-}
 
 export default Nav;
