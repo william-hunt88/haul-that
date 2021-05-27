a3419038cdfd8fef26a6b77684e9cc956fb106b7
Your NameAlex <alehr45@gmail.com>
2021-05-27 09:36:08 -0500

    develop

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,32 +1,35 @@
 import React from "react";
-import { Card, ListGroupItem, ListGroup, Container, Col , Row } from "react-bootstrap";
+import {
+  Card,
+  ListGroupItem,
+  ListGroup,
+  Container,
+  Col,
+  Row,
+} from "react-bootstrap";
 import image from "../images/profile.png";
 
 const Profile = () => {
   return (
-    <Container className='profileForm'>
-    <Row>
-      
-      <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src={ image } />
-  <Card.Body>
-    <Card.Title>John Doe</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-  </Card.Body>
-  <ListGroup className="list-group-flush">
-    <ListGroupItem>Rating ☆☆☆☆☆</ListGroupItem>
-    <ListGroupItem>Completed Deliveries: 0</ListGroupItem>
-    <ListGroupItem>Vestibulum at eros</ListGroupItem>
-  </ListGroup>
-
-</Card>
-
-   
+    <Container className="profileForm">
+      <Row>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={image} />
+          <Card.Body>
+            <Card.Title>John Doe</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroupItem>Rating ☆☆☆☆☆</ListGroupItem>
+            <ListGroupItem>Completed Deliveries: 0</ListGroupItem>
+          </ListGroup>
+        </Card>
       </Row>
-    </Container>)
+    </Container>
+  );
 };
 
 export default Profile;
