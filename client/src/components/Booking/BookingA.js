ace6062fadd35d21a623a8a8b077a3969d93af90
Your NameAlex <alehr45@gmail.com>
2021-05-28 10:36:05 -0500

    merge

--- client/src/components/Booking/BookingA.js
+++ client/src/components/Booking/BookingA.js
@@ -18,7 +18,7 @@ const BookingA = () => {
     cityD: "",
     stateD: "",
     zipD: "",
-    });
+  });
 
   const [addJob] = useMutation(ADD_JOB);
 
@@ -29,8 +29,8 @@ const BookingA = () => {
   // update state based on form input changes
   const handleChange = (event) => {
     const { name, value } = event.target;
-console.log(name)
-console.log(typeof value)
+    console.log(name);
+    console.log(typeof value);
     setFormState({
       ...formState,
       [name]: value,
@@ -189,6 +189,55 @@ console.log(typeof value)
                   <option>Choose...</option>
                   <option>...</option>
                   <option>Alabama</option>
+                  <option>Alaska</option>
+                  <option>Arizona</option>
+                  <option>Arkansas</option>
+                  <option>California</option>
+                  <option>Colorado</option>
+                  <option>Connecticut</option>
+                  <option>Delaware</option>
+                  <option>Florida</option>
+                  <option>Georgia</option>
+                  <option>Hawaii</option>
+                  <option>Idaho</option>
+                  <option>Illinois</option>
+                  <option>Indiana</option>
+                  <option>Iowa</option>
+                  <option>Kansas</option>
+                  <option>Kentucky</option>
+                  <option>Louisiana</option>
+                  <option>Maine</option>
+                  <option>Maryland</option>
+                  <option>Massachusetts</option>
+                  <option>Michigan</option>
+                  <option>Minnesota</option>
+                  <option>Mississippi</option>
+                  <option>Missouri</option>
+                  <option>Montana</option>
+                  <option>Nebraska</option>
+                  <option>Nevada</option>
+                  <option>New Hampshire</option>
+                  <option>New Jersey</option>
+                  <option>New Mexico</option>
+                  <option>New York</option>
+                  <option>North Carolina</option>
+                  <option>North Dakota</option>
+                  <option>Ohio</option>
+                  <option>Oklahoma</option>
+                  <option>Oregon</option>
+                  <option>Pennsylvania</option>
+                  <option>Rhode Island</option>
+                  <option>South Carolina</option>
+                  <option>South Dakota</option>
+                  <option>Tennessee</option>
+                  <option>Texas</option>
+                  <option>Utah</option>
+                  <option>Vermont</option>
+                  <option>Virginia</option>
+                  <option>Washington</option>
+                  <option>West Virginia</option>
+                  <option>Wisconsin</option>
+                  <option>Wyoming</option>
                 </Form.Control>
               </Form.Group>
 
@@ -234,6 +283,55 @@ console.log(typeof value)
                   <option>Choose...</option>
                   <option>...</option>
                   <option>Alabama</option>
+                  <option>Alaska</option>
+                  <option>Arizona</option>
+                  <option>Arkansas</option>
+                  <option>California</option>
+                  <option>Colorado</option>
+                  <option>Connecticut</option>
+                  <option>Delaware</option>
+                  <option>Florida</option>
+                  <option>Georgia</option>
+                  <option>Hawaii</option>
+                  <option>Idaho</option>
+                  <option>Illinois</option>
+                  <option>Indiana</option>
+                  <option>Iowa</option>
+                  <option>Kansas</option>
+                  <option>Kentucky</option>
+                  <option>Louisiana</option>
+                  <option>Maine</option>
+                  <option>Maryland</option>
+                  <option>Massachusetts</option>
+                  <option>Michigan</option>
+                  <option>Minnesota</option>
+                  <option>Mississippi</option>
+                  <option>Missouri</option>
+                  <option>Montana</option>
+                  <option>Nebraska</option>
+                  <option>Nevada</option>
+                  <option>New Hampshire</option>
+                  <option>New Jersey</option>
+                  <option>New Mexico</option>
+                  <option>New York</option>
+                  <option>North Carolina</option>
+                  <option>North Dakota</option>
+                  <option>Ohio</option>
+                  <option>Oklahoma</option>
+                  <option>Oregon</option>
+                  <option>Pennsylvania</option>
+                  <option>Rhode Island</option>
+                  <option>South Carolina</option>
+                  <option>South Dakota</option>
+                  <option>Tennessee</option>
+                  <option>Texas</option>
+                  <option>Utah</option>
+                  <option>Vermont</option>
+                  <option>Virginia</option>
+                  <option>Washington</option>
+                  <option>West Virginia</option>
+                  <option>Wisconsin</option>
+                  <option>Wyoming</option>
                 </Form.Control>
               </Form.Group>
 
--- client/src/components/Jobs.js
+++ client/src/components/Jobs.js
@@ -5,37 +5,34 @@ import { Container } from "react-bootstrap";
 import { useQuery } from "@apollo/react-hooks";
 import { GET_JOBS } from "../utils/queries";
 
-
 const Jobs = () => {
-  const {data} = useQuery(GET_JOBS);
-  if (data){console.log(data)}
+  const { data } = useQuery(GET_JOBS);
+  if (data) {
+    console.log(data.jobs);
+  }
 
   return (
     <div>
       <Map />
+      <Container className="jobForm">
+        <Card className="cardbody" style={{ width: "17rem" }}>
+          <Card.Body>
+            <Card.Title>Job #1</Card.Title>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroupItem>Description: </ListGroupItem>
+            <ListGroupItem>Item Category: </ListGroupItem>
+            <ListGroupItem>Price: </ListGroupItem>
+          </ListGroup>
+          <Card.Body>
+            <Button variant="danger">Accept Job</Button>{" "}
+          </Card.Body>
+        </Card>
+      </Container>
 
-      <Container className='jobForm'>
-      
-       
-      <Card className="cardbody" style={{ width: "17rem" }}>
-        <Card.Body>
-          <Card.Title>Job #1</Card.Title>
-        </Card.Body>
-        <ListGroup className="list-group-flush">
-          <ListGroupItem>Distance: </ListGroupItem>
-          <ListGroupItem>Item Category: </ListGroupItem>
-          <ListGroupItem>Price: </ListGroupItem>
-        </ListGroup>
-        <Card.Body>
-          <Button variant="danger">Accept Job</Button>{" "}
-        </Card.Body>
-      </Card>
-      
-        
-      
-    </Container>
+  
     </div>
   );
-}
+};
 
 export default Jobs;
