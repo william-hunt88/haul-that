e1a288f79fb41c3b14bff1960108de63b4e69e72
w.hunt <wfh2d88@gmail.com>
2021-05-30 09:45:16 -0500

    heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/haul-that",
+  process.env.MONGODB_URI ,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
